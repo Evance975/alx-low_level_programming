587b087bacd36d0d5efdca7ebfa8faef517ec36f
Evance975 <evanceomolo975@gmail.com>
2023-06-15 00:53:04 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this program prints a line with a puts function
+ * Return: Returns 0; always success
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
