df635958a84bcb09254ee4ad581c13b300f7a676
Evance975 <evanceomolo975@gmail.com>
2023-06-15 02:01:00 -0700

    Writting into standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - printing to the standard error
+ * Return: Returns 1 when successfull
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
