5861b7d84a33a1b67a8177183311b7d999152709
Evance975 <evanceomolo975@gmail.com>
2023-08-07 16:17:28 -0700

    final commit

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,7 +4,12 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <unistd.h>
+
+void error_98(int fd_to, char *buffer, char *argv);
+void error_99(int fd_to, char *buffer, char *argv);
+void error_100(int fd_t0, char *buffer);
 int append_text_to_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+
 #endif
